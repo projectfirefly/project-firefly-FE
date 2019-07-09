29717e9819867204ba5dcf12132fbe4d73af97c0
Andrew Benedict <atbenedict@gmail.com>
2019-07-09 17:32:11 -0400

    added components

---
+++ firefly/src/components/ActionPane.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActionPane = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ActionPane
---
+++ firefly/src/components/ProgramPane.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProgramPane = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProgramPane
