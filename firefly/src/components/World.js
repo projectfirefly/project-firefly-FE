f677e09f1e4148842fc3cfd8b3ab57b7e2b20171
Robert Driskell <driskell.ba@gmail.com>
2019-07-11 15:41:51 -0500

    Generating dots(fireflies) via an array, this should be saveable to the server via json stringify

--- firefly/src/components/World.js
+++ firefly/src/components/World.js
@@ -3,23 +3,82 @@ import { Stage, Circle, Layer } from 'react-konva';
 import Konva from 'konva';
 
 export class World extends Component {
-
-    move() {
-        
+    state = {
+        targets: [
+            {
+                x: 400,
+                y: 200,
+                color: '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+            },
+            {
+                x: ~~(Math.random() * 500),
+                y: ~~(Math.random() * 500),
+                color: '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+            },
+            {
+                x: ~~(Math.random() * 500),
+                y: ~~(Math.random() * 500),
+                color: '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+            },
+            {
+                x: ~~(Math.random() * 500),
+                y: ~~(Math.random() * 500),
+                color: '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+            },
+        ]
     }
 
     render() {
         return (
-            <div>
-                <Stage width={window.innerWidth*.48} height={window.innerHeight*.90} style={{border: '10px solid black'}}>
+            <div id="stage-container">
+                <Stage container={"stage-container"} width={window.innerWidth * .48} height={window.innerHeight * .90} style={{ border: '10px solid black' }}>
                     <Layer>
-                        <Circle x={window.innerWidth*.48*.5} y={window.innerHeight*.98*.5} draggable={true} radius={6} fill={"black"}/>
-                        <Circle x={window.innerWidth*.48*.5} y={window.innerHeight*.98*.5} draggable={true} radius={6} fill={"red"}/>
-                        <Circle x={window.innerWidth*.48*.5} y={window.innerHeight*.98*.5} draggable={true} radius={6} fill={"blue"}/>
-                        <Circle x={window.innerWidth*.48*.5} y={window.innerHeight*.98*.5} draggable={true} radius={6} fill={"green"}/>
+                        {this.state.targets.map((target) => (
+                            <Target x={target.x} y={target.y} color={target.color} key={target.x} />
+                        ))}
                     </Layer>
                 </Stage>
             </div>
+        )
+    }
+}
+
+class Target extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            x: props.x,
+            y: props.y,
+            color: props.color
+        }
+    }
+
+    handleDragEnd = (e) => {
+        this.setState({
+            x: e.target.x(),
+            y: e.target.y()
+        })
+        console.log(JSON.stringify(this.state));
+        var a = 100;
+        var b = 200;
+        a = a ^ b;
+        b = b ^ a;
+        a = a ^ b;
+        console.log(a);
+        console.log(b);
+    }
+
+    render() {
+        return (
+            <Circle
+                x={this.state.x}
+                y={this.state.y}
+                draggable
+                onDragEnd={this.handleDragEnd}
+                radius={6}
+                fill={this.props.color}
+            />
         )
     }
 }
