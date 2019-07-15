e29ff41375debfe1259eca8e0f64da70ff7fbf45
aaharbaugh <aaronmharbaugh@gmail.com>
2019-07-15 16:41:01 -0500

    google analytics code block seperated with comments

--- firefly/src/App.js
+++ firefly/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
 import FormikApp from "./components/LoginForm";
+
+//google analytics code start//
 import ReactGA from 'react-ga';
 ReactGA.initialize('UA-143905861-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
+//google analytics code end//
 
 function App() {
   return (
