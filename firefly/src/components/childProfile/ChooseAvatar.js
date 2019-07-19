4240d38d4ed55a16a6230e1cdd0f90496b853b0d
Andrew Benedict <atbenedict@gmail.com>
2019-07-19 08:53:46 -0400

    created custom color ability for firefly

--- firefly/src/components/childProfile/ChooseAvatar.js
+++ firefly/src/components/childProfile/ChooseAvatar.js
@@ -1,12 +1,18 @@
 import React from "react";
+import ColorSlider from "../ColorSlider/ColorSlider";
 import Icon from "../../assets/icons";
+import styled from "styled-components";
 
+const Chooser = styled.div`
+  margin: 0 auto;
+  margin-top: 50px;
+`;
 const ChooseAvatar = () => {
   return (
-    <div>
-      <h2>Choose your Avatar</h2>
+    <Chooser>
       <Icon name="Firefly" width={200} />
-    </div>
+      <ColorSlider />
+    </Chooser>
   );
 };
 
