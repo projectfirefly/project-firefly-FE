322452293316123439fc0c047f0180f55c087a34
Robert Driskell <driskell.ba@gmail.com>
2019-07-19 10:11:50 -0500

    Color slider is now a rainbow

--- firefly/src/components/ColorSlider/ColorSlider.jsx
+++ firefly/src/components/ColorSlider/ColorSlider.jsx
@@ -17,22 +17,40 @@ export default function ColorSlider() {
         track: {
             height: 8,
             borderRadius: 5,
+            display: 'none',
         },
         rail: {
             height: 8,
             borderRadius: 5,
+            background: `linear-gradient(to right, hsl(0,70%,50%),
+            hsl(${(360) / 5},70%,50%),
+            hsl(${(360 * 2) / 5},70%,50%),
+            hsl(${(360 * 3) / 5},70%,50%),
+            hsl(${(360 * 4) / 5},70%,50%),
+            hsl(${360},70%,50%))`,
+            opacity: '.7',
         },
         thumb: {
             height: 24,
             width: 24,
-            backgroundColor: '#fff',
-            border: '2px solid currentColor',
+            backgroundColor: 'currentColor',
+            border: '5px solid currentColor',
             marginTop: -8,
             marginLeft: -12,
             '&:focus,&:hover,&$active': {
                 boxShadow: 'inherit',
             },
         },
+        active: {
+            height: 48,
+            width: 48,
+            marginTop: -18,
+            marginLeft: -24,
+        },
+        containerDiv: {
+            padding: '30px',
+            backgroundColor: '#454a54',
+        }
     })
 
     const classes = useStyles();
@@ -42,11 +60,13 @@ export default function ColorSlider() {
     };
 
     return (
-        <div >
+        <div>
             <Typography id="continuous-slider" gutterBottom>
                 Color
             </Typography>
-            <Slider classes={classes} value={value} max="360" onChange={handleChange} aria-label="Pretto slider" />
+            <div className={classes.containerDiv}>
+                <Slider classes={classes} value={value} max={360} onChange={handleChange} aria-label="Pretto slider" />
+            </div>
         </div>
     );
 }
