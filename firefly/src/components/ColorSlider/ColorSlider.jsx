cadf712901564fd203d751059edd59a86e5aba60
danpatrickoneill <dan47573@gmail.com>
2019-07-19 16:39:30 -0400

    Deleted duplicate handleChange function

--- firefly/src/components/ColorSlider/ColorSlider.jsx
+++ firefly/src/components/ColorSlider/ColorSlider.jsx
@@ -9,67 +9,69 @@ import Tooltip from "@material-ui/core/Tooltip";
 export default function ColorSlider() {
   const [value, setValue] = React.useState(150);
 
-    const useStyles = makeStyles({
-        root: {
-            color: `hsl(${value},100%,50%)`,
-        },
-        track: {
-            height: 8,
-            borderRadius: 5,
-            display: 'none',
-        },
-        rail: {
-            height: 8,
-            borderRadius: 5,
-            background: `linear-gradient(to right, hsl(0,70%,50%),
-            hsl(${(360) / 5},70%,50%),
+  const useStyles = makeStyles({
+    root: {
+      color: `hsl(${value},100%,50%)`
+    },
+    track: {
+      height: 8,
+      borderRadius: 5,
+      display: "none"
+    },
+    rail: {
+      height: 8,
+      borderRadius: 5,
+      background: `linear-gradient(to right, hsl(0,70%,50%),
+            hsl(${360 / 5},70%,50%),
             hsl(${(360 * 2) / 5},70%,50%),
             hsl(${(360 * 3) / 5},70%,50%),
             hsl(${(360 * 4) / 5},70%,50%),
             hsl(${360},70%,50%))`,
-            opacity: '.7',
-        },
-        thumb: {
-            height: 24,
-            width: 24,
-            backgroundColor: 'currentColor',
-            border: '5px solid currentColor',
-            marginTop: -8,
-            marginLeft: -12,
-            '&:focus,&:hover,&$active': {
-                boxShadow: 'inherit',
-            },
-        },
-        active: {
-            height: 48,
-            width: 48,
-            marginTop: -18,
-            marginLeft: -24,
-        },
-        containerDiv: {
-            padding: '30px',
-            backgroundColor: '#454a54',
-        }
-    })
-        
+      opacity: ".7"
+    },
+    thumb: {
+      height: 24,
+      width: 24,
+      backgroundColor: "currentColor",
+      border: "5px solid currentColor",
+      marginTop: -8,
+      marginLeft: -12,
+      "&:focus,&:hover,&$active": {
+        boxShadow: "inherit"
+      }
+    },
+    active: {
+      height: 48,
+      width: 48,
+      marginTop: -18,
+      marginLeft: -24
+    },
+    containerDiv: {
+      padding: "30px",
+      backgroundColor: "#454a54"
+    }
+  });
+
   const classes = useStyles();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-    
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
 
-    return (
-        <div>
-            <Typography id="continuous-slider" gutterBottom>
-                Color
-            </Typography>
-            <div className={classes.containerDiv}>
-                <Slider classes={classes} value={value} max={360} onChange={handleChange} aria-label="Pretto slider" />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Typography id="continuous-slider" gutterBottom>
+        Color
+      </Typography>
+      <div className={classes.containerDiv}>
+        <Slider
+          classes={classes}
+          value={value}
+          max={360}
+          onChange={handleChange}
+          aria-label="Pretto slider"
+        />
+      </div>
+    </div>
+  );
 }
