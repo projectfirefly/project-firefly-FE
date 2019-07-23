a332fa266fe38868032dde8baf537dfdb5cd0f8b
aaharbaugh <aaronmharbaugh@gmail.com>
2019-07-23 11:53:13 -0500

    fixed up styling of the boxes on child profiles

--- firefly/src/components/ChildProfiles/ChildProfiles.jsx
+++ firefly/src/components/ChildProfiles/ChildProfiles.jsx
@@ -24,104 +24,53 @@ const ChildProfiles = () => {
   const useStyles = makeStyles({
     header: {
       fontSize: "34px",
-      letterSpacing: "7",
+      letterSpacing: "10px",
       fontWeight: "900",
       color: "#5B4EFF",
       width: "100%",
       display: "flex",
-      justifyContent: "center"
+      justifyContent: "center",
+      marginBottom: '50px',
     },
     root: {
       display: "flex",
       flexDirection: "row",
       flexWrap: "wrap",
-      maxWidth: "100%",
       padding: "60px"
     },
     cardContainer: {
       display: "flex",
       flexWrap: "wrap",
-      justifyContent: "center",
+      justifyContent: "flex-start",
       alignItems: "center",
-      height: "100%",
-      width: "100%"
-    },
-    cardDashed: {
-      display: "flex",
-      width: "18%",
-      margin: "4%",
-      height: "365px",
-      flexShrink: 0,
-      border: "2px solid #ABB0BA",
-      borderRadius: "10px",
-      boxShadow: "0px 3px #8F96A3"
+      paddingLeft: "120px"
     },
     card: {
-      display: "flex",
-      width: "18%",
-      margin: "4%",
-      flexShrink: 0,
       border: "2px solid #ABB0BA",
       borderRadius: "10px",
-      boxShadow: "0px 3px #8F96A3"
-    },
-    media: {
-      height: "50%"
-    },
-    cardActionArea: {
-      display: "flex",
-      height: "100%",
-      width: "100%",
-      justifyContent: "center",
-      alignItems: "center"
-    },
-    cardContentPlus: {
-      display: "flex",
-      flexDirection: "column",
-      width: "100%",
-      height: "100%",
-      justifyContent: "center",
-      alignItems: "center"
+      boxShadow: "0px 3px #8F96A3",
+      margin: '45px 80px'
     },
     cardContent: {
       display: "flex",
       flexDirection: "column",
-      width: "100%",
-      height: "100%",
-      justifyContent: "space-between",
-      alignItems: "center"
-    },
-    plus: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "4rem",
-      height: "4rem",
-      padding: "1.5rem",
-      borderRadius: "50%",
-      backgroundColor: "#1AAE9F",
-      color: "white",
-      fontSize: "6rem"
+      alignItems: 'center',
+      margin: '-17px -65px -40px -65px'
     },
     text: {
       display: "flex",
       flexDirection: "column",
-      alignItems: "center"
+      alignItems: "center", 
+      marginBottom: '-25px',
+      paddingTop: '10px'
     },
     name: {
-      fontSize: "3rem",
-      color: "#1AAE9F"
+      fontSize: "1.5rem",
+      color: "#243d14"
     },
-    level: {
-      fontSize: "2rem",
-      paddingBottom: "2rem"
-    },
-    avatar: {
-      width: "100%",
-      height: "100%"
-    },
-    slider: {
-      width: "100%"
+    flysize: {
+        width: '75%',
+        height: '75%'
     }
   });
 
@@ -129,48 +78,26 @@ const ChildProfiles = () => {
 
   return (
     <div className={classes.root}>
-      {/* <AppBar className={classes.appBar} position="static">
-                <Toolbar>
-                    Hi, {childProfileState.profiles.find((profile) => {
-                        return childProfileState.selected.id === profile.id;
-                    }).name}
-                </Toolbar>
-            </AppBar> */}
       <div className={classes.header}> CHOOSE YOUR FIREFLY </div>
       <div className={classes.cardContainer}>
         {childProfileState.profiles.map(profile => (
           <Card className={classes.card}>
             <CardActionArea
-              className={classes.cardActionArea}
               onClick={() =>
                 dispatch({ type: "UPDATE_SELECTED", payload: profile.id })
               }
             >
               <CardContent className={classes.cardContent}>
-                <ProfileFly color={profile.color} />
-                {/* <img src={logo} className={classes.avatar} /> */}
-                <div className={classes.text}>
-                  <h2 className={classes.name}>{profile.name}</h2>
-                </div>
-                {/* <ColorSlider className={classes.slider} value={profile.color} updateColor={(val) => {
-                                    dispatch({
-                                        type: 'UPDATE_COLOR', payload: {
-                                            ...profile,
-                                            color: val,
-                                        }
-                                    })
-                                }} /> */}
+                    <div className={classes.text}>
+                        <h2 className={classes.name}>{profile.name}</h2>
+                    </div>
+                    <div className={classes.flysize}>
+                        <ProfileFly  color={profile.color} />
+                    </div>
               </CardContent>
             </CardActionArea>
           </Card>
         ))}
-        <Card className={classes.cardDashed}>
-          <CardActionArea className={classes.cardActionArea}>
-            <CardContent className={classes.cardContentPlus}>
-              <h3 className={classes.plus}>+</h3>
-            </CardContent>
-          </CardActionArea>
-        </Card>
       </div>
     </div>
   );
