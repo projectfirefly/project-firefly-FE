40481302da36bb0faf87959f90eac838f4c7e0ae
Robert Driskell <driskell.ba@gmail.com>
2019-07-23 18:23:39 -0500

    Moved color slider over, got reducers and whatever done

--- firefly/src/components/childProfile/ChooseNameEtc.js
+++ firefly/src/components/childProfile/ChooseNameEtc.js
@@ -1,22 +1,24 @@
 import React from "react";
+import ColorSlider from "../ColorSlider/ColorSlider";
 
-const ChooseNameEtc = () => {
-  return (
-    <div className="">
-      <div style={{}}>
-        <h2 className="inputLabel">Nickname</h2>
-      </div>
-      <input className="input" type="text" />
+const ChooseNameEtc = (props) => {
+    return (
+        <div className="">
+            <div style={{}}>
+                <h2 className="inputLabel">Nickname</h2>
+            </div>
+            <input className="input" type="text" />
 
-      <div>
-        <h2>Accessories</h2>
-      </div>
+            <div>
+                <h2>Accessories</h2>
+            </div>
 
-      <div>
-        <h2>Light Color</h2>
-      </div>
-    </div>
-  );
+            <div>
+                <h2>Light Color</h2>
+                <ColorSlider value={props.colorValue} updateColor={props.updateColor} />
+            </div>
+        </div>
+    );
 };
 
 export default ChooseNameEtc;
--- firefly/src/context/ChildProfiles/ChildProfileStore.jsx
+++ firefly/src/context/ChildProfiles/ChildProfileStore.jsx
@@ -4,47 +4,44 @@ export const childContext = React.createContext();
 
 const initialState = {
     profiles: [
-    {
-        id: 0,
-        name: 'George',
-        avatar: "G",
-        color: "120",
-    },
-    {
-        id: 1,
-        name: 'Jorge',
-        avatar: "J",
-        color: "0",
-    },
-    {
-        id: 2,
-        name: "Gyorg",
-        avatar: "Gy",
-        color: "320",
-    },
-    {
-        id: 3,
-        name: "John",
-        avatar: "Jo",
-        color: "60",
-    },
-    {
-        id: 4,
-        name: "Jacob",
-        avatar: "Ja",
-        color: "250",
-    }],
+        {
+            id: 0,
+            name: 'George',
+            color: "120",
+        },
+        {
+            id: 1,
+            name: 'Jorge',
+            color: "0",
+        },
+        {
+            id: 2,
+            name: "Gyorg",
+            color: "320",
+        },
+        {
+            id: 3,
+            name: "John",
+            color: "60",
+        },
+        {
+            id: 4,
+            name: "Jacob",
+            color: "250",
+        }],
     selected: {
         id: 2,
     }
 }
 
-export const UPDATE_COLOR = 'UPDATE_COLOR';
 export const UPDATE_SELECTED = 'UPDATE_SELECTED';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 
 function reducer(state, action) {
-    switch(action.type) {
-        case UPDATE_COLOR:
+    switch (action.type) {
+        case UPDATE_SELECTED:
+            return { ...state, selected: { id: action.payload } }
+        case UPDATE_PROFILE:
             const newArr = state.profiles.map((profile) => {
                 if (profile.id === action.payload.id) {
                     return action.payload
@@ -52,9 +49,7 @@ function reducer(state, action) {
                     return profile
                 }
             })
-            return {...state, profiles: newArr};
-        case UPDATE_SELECTED:
-            return {...state, selected: { id: action.payload }}
+            return { ...state, profiles: newArr };
         default:
             throw Error('reducer error');
     }
