c535c50208e99360c1ad83d56c90ff62e728e7e0
Andrew Benedict <atbenedict@gmail.com>
2019-07-24 14:59:59 -0400

    updated SVG for firefly

--- firefly/src/components/childProfile/ChooseAvatar.js
+++ firefly/src/components/childProfile/ChooseAvatar.js
@@ -7,24 +7,21 @@ import Slider from "@material-ui/core/Slider";
 const Chooser = styled.div`
   margin: 0 auto;
   width: 100%;
-  margin-top: -50px;
 `;
 
 const SliderBox = styled.div`
   margin: 0 auto;
   width: 200px;
-  margin-top: -50px;
+
   padding-bottom: 20px;
 `;
 
 export default function ChooseAvatar(props) {
-
   return (
     <Chooser>
       <Icon
         name="Firefly"
         width={"100%"}
-        height={"100%"}
         viewBox={"0 0 1024 1024"}
         lighttopFill={`hsl(${props.value},100%,35%)`}
         lightmidFill={`hsl(${props.value},100%,45%)`}
