a8149391b68dedafd4724182bdf4246f32d7d3a6
Andrew Benedict <atbenedict@gmail.com>
2019-07-24 18:05:41 -0400

    working on accessory carousel and bringing acessory SVG icons into app

--- firefly/src/assets/icons/index.js
+++ firefly/src/assets/icons/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Firefly from "./firefly/Firefly";
 import ProfileFly from "./ProfileFly";
+import LambdaHat from "./accessories/LambdaHat";
 
 const Icon = props => {
   switch (props.name) {
     case "Firefly":
       return <Firefly {...props} />;
+    case "LambdaHat":
+      return <LambdaHat {...props} />;
     case "ProfileFly":
       return <ProfileFly {...props} />;
     default:
