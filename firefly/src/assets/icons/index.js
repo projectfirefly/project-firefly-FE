c543a8e798039212ecc96a8f28e7d6faa8315ed0
Andrew Benedict <atbenedict@gmail.com>
2019-07-24 21:15:28 -0400

    created accessory carousel with custom sized svg elements

--- firefly/src/assets/icons/index.js
+++ firefly/src/assets/icons/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import Firefly from "./firefly/Firefly";
 import ProfileFly from "./ProfileFly";
 import LambdaHat from "./accessories/LambdaHat";
+import LambdaHatSM from "./accessories/LambdaHatSM";
+import NerdGlasses from "./accessories/NerdGlasses";
+import NerdGlassesSM from "./accessories/NerdGlassesSM";
+import PinkHeadphone from "./accessories/PinkHeadphone";
+import PinkHeadphoneSM from "./accessories/PinkHeadphoneSM";
+import SunGlasses from "./accessories/SunGlasses";
+import SunGlassesSM from "./accessories/SunGlassesSM";
+import LeftArrow from "./LeftArrow";
+import RightArrow from "./RightArrow";
 
 const Icon = props => {
   switch (props.name) {
@@ -9,8 +18,26 @@ const Icon = props => {
       return <Firefly {...props} />;
     case "LambdaHat":
       return <LambdaHat {...props} />;
+    case "LambdaHatSM":
+      return <LambdaHatSM {...props} />;
+    case "NerdGlasses":
+      return <NerdGlasses {...props} />;
+    case "NerdGlassesSM":
+      return <NerdGlassesSM {...props} />;
+    case "PinkHeadphone":
+      return <PinkHeadphone {...props} />;
+    case "PinkHeadphoneSM":
+      return <PinkHeadphoneSM {...props} />;
+    case "SunGlasses":
+      return <SunGlasses {...props} />;
+    case "SunGlassesSM":
+      return <SunGlassesSM {...props} />;
     case "ProfileFly":
       return <ProfileFly {...props} />;
+    case "LeftArrow":
+      return <LeftArrow {...props} />;
+    case "RightArrow":
+      return <RightArrow {...props} />;
     default:
       return <div />;
   }
