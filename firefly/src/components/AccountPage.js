627c1981089a059c54e1351d7eb38d770a7b171b
Leslie Thompson <lcrthompson@gmail.com>
2019-07-25 15:18:22 -0700

    added edit button, resized cards, adjust color, account info

--- firefly/src/components/AccountPage.js
+++ firefly/src/components/AccountPage.js
@@ -4,50 +4,82 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import "../styles/accountPage.scss";
+import { Link } from "react-router-dom";
 
-// import { Link } from "react-router-dom";
-// import image1 from "../../assets/icons/Firefly.svg";
-// import { FaPen } from "react-icons/fa";
+import image1 from "../assets/icons/Firefly.svg";
+import { FaPen } from "react-icons/fa";
 
 const useStyles = makeStyles(theme => ({
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(5),
     textAlign: "left",
-    color: theme.palette.text.secondary
+    color: theme.palette.text.secondary,
+    borderRadius: " 20px"
   }
 }));
 
 export default function ProfileView() {
   const classes = useStyles();
+
   return (
     <Container className="root" maxWidth="lg">
       <h1 className="header">My Account</h1>
 
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
+      <Grid container spacing={4}>
+        <Grid item xs={7}>
           <Paper className={classes.paper}>
             <h2 className="sectionHeader">Account Information</h2>
-            <p>Email </p>
-            <p>Name </p>
-            <p>Address </p>
+            <Grid container spacing={3}>
+              <Grid item xs={2}>
+                <div className="infoLabel">Email</div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="userInfo">Users Email</div>
+              </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid item xs={2}>
+                <div className="infoLabel">Name</div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="userInfo">Users Name</div>
+              </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid item xs={2}>
+                <div className="infoLabel">Address</div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="userInfo">Users Address</div>
+              </Grid>
+            </Grid>
           </Paper>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={4} className="rightCards">
           <Paper className={classes.paper}>
             <h2 className="sectionHeader">Manage Profile</h2>
+            <div className="firefly">
+              <p>New Profile</p>
+              {/* <img src={image1} alt="users profile" /> */}
+              <div className="edit">
+                <FaPen />
+                Edit
+              </div>
+              <p>Username</p>
+            </div>
           </Paper>
+          <div className="button">
+            <Link style={{ color: "white", textDecoration: "none" }} to="">
+              Back to Game
+            </Link>
+          </div>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={7}>
           <Paper className={classes.paper}>
             <h2 className="sectionHeader"> Payment Information</h2>
           </Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-            <button>Back To Game</button>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={7}>
           <Paper className={classes.paper}>
             <h2 className="sectionHeader">
               Educational Research Participation
