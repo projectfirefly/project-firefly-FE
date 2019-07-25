642b9e18f8858624ff9f57e0deef4ae88f34b5a2
Andrew Benedict <atbenedict@gmail.com>
2019-07-25 19:10:09 -0400

    Quick update

--- firefly/src/components/ChildProfiles/ChildProfiles.jsx
+++ firefly/src/components/ChildProfiles/ChildProfiles.jsx
@@ -1,21 +1,11 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { childContext } from "../../context/ChildProfiles/ChildProfileStore";
 
 //Material-UI
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import AppBar from "@material-ui/core/AppBar";
-
-import ChildProfileDialog from "./ChildProfileDialog";
-import logo from "../../assets/icons/Firefly.svg";
-import { Toolbar } from "@material-ui/core";
-import ColorSlider from "../ColorSlider/ColorSlider";
 import ProfileFly from "../../assets/icons/ProfileFly";
 
 const ChildProfiles = () => {
