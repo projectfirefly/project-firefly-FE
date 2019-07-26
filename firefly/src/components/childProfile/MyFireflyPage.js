29f802031f5ef550b8aa54290082c4712353f42a
aaharbaugh <aaronmharbaugh@gmail.com>
2019-07-26 14:28:10 -0500

    changes to the edit firefly profile

--- firefly/src/components/childProfile/MyFireflyPage.js
+++ firefly/src/components/childProfile/MyFireflyPage.js
@@ -11,88 +11,78 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
     flexDirection: "column",
+    width: "98%",
     alignItems: "center",
     justifyContent: "center",
-    width: "98%",
-    marginTop: "3%",
-    fontFamily: "nunito"
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary
+    height: "100vh"
   },
   header: {
+    height: '5%',
     textAlign: "center",
     color: "#5B4EFF",
-    fontSize: "50px",
+    fontSize: "28px",
     letterSpacing: "7px",
-    fontWeight: "900",
+    fontWeight: "bold",
     textTransform: "uppercase",
     fontFamily: "nunito",
-    marginBottom: "10px"
   },
   mainBody:{
     display: "flex",
-    width: "98%",
+    width: "100%",
+    height: "60%",
+    padding: "4%",
   },
   leftParent:{
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    width: "60%",
+    width: "50%",
   },
   leftContainer: {
-    width: "50%",
+    width: "55%",
+    height: '80%',
     background: "white",
     borderRadius: "20px",
     boxShadow: "0px 2px 4px #000000"
-  },
-  firefly: {
-    width: "200px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
   },
   fireflyContainer: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center"
   },
+  firefly: {
+    width: "200px",
+  },
   rightContainer: {
-    // background: "white",
-    borderRadius: " 20px",
-    // boxShadow: "0px 2px 4px #000000"
+    width: '50%',
   },
   rightCards: {
-    textAlign: "center",
+    width: '80%',
+    height: "150px",
     border: "solid #ABB0BA 2px",
     borderRadius: "10px",
     backgroundColor: "#FFFFFF",
     boxShadow: "0px 3px #8F96A3",
-    height: "37%",
-    color: "#5B4EFF",
-    fontWeight: "bold",
-    justifyContent: "center",
-    alignItems: "center",
-    // marginLeft: "10%"
+    marginBottom: '40px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   rightCardContent: {
     display: "flex",
-    paddingTop: "10%",
-    width: "100%",
-    margin: "0 15%"
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   rightCardsText: {
-    fontSize: "30px",
-    paddingTop: "5%",
-    marginLeft: "8%"
+    color: "#5B4EFF",
+    fontSize: "24px",
+    fontWeight: '700'
+  },
+  rightCardsImg: { 
+    width: '15%'
   },
   bottomCard: {
-     marginTop: "13%" 
-    },
-  rightCardsImg: { 
-    width: "10%" 
   },
   chooseFirefly: {
     textAlign: "center",
@@ -108,24 +98,34 @@ const useStyles = makeStyles(theme => ({
     alignItems: "center",
     justifyContent: "center"
   },
+  pushRight: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end'
+  },
   edit: {
-    width: "100%",
-    textAlign: "right",
     color: "#4AA810",
-    fontWeight: "bold",
-    fontSize: "20px"
+    marginLeft: '3px'
+  },
+  editContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: "34px",
+    height: "30px",
+    border: "solid #ABB0BA 2px",
+    borderRadius: "10px",
+    boxShadow: "0px 3px #8F96A3",
+    margin: '10px'
   },
   username: {
     textAlign: "center",
     color: "#152F04",
     fontWeight: "bold",
-    fontSize: "22px"
+    fontSize: "18px"
   },
-  editContainer: {
-    marginBottom: " 5%",
-    marginRight: "5%",
-    paddingTop: "3%"
-  }
+
 }));
 
 export default function MyFireflyPage() {
@@ -133,21 +133,26 @@ export default function MyFireflyPage() {
 
   return (
     <React.Fragment>
-      <Container className={classes.root} maxWidth="lg" component="div">
+      <Container className={classes.root} component="div">
+
         <h1 className={classes.header}>My Firefly</h1>
 
         <div className={classes.mainBody} >
-            <div className={classes.leftParent}>
+
+            <div className={classes.leftParent}> 
+            {/* this div has invisible margins to keep container center */}
+
               <div className={classes.leftContainer}>
-                <div className={classes.editContainer}>
-                  <Link style={{ textDecoration: "none" }} to="/createprofile">
-                    <div className={classes.edit}>
-                      <FaPen style={{ marginRight: "5px" }} />
-                      Edit
-                    </div>
-                  </Link>
+                <div className={classes.pushRight}>
+                  <div className={classes.editContainer}>
+                    <Link to="/createprofile">
+                      <div className={classes.edit}>
+                        <FaPen style={{ marginRight: "5px" }} />
+                      </div>
+                    </Link>
+                  </div>
                 </div>
-                <h3 className={classes.username}>Users name</h3>
+                <h3 className={classes.username}>Username</h3>
                 <div className={classes.fireflyContainer}>
                   <img
                     className={classes.firefly}
@@ -183,7 +188,9 @@ export default function MyFireflyPage() {
               </div>
             </div>
           </div>
+
         </div>
+
       </Container>
     </React.Fragment>
   );
