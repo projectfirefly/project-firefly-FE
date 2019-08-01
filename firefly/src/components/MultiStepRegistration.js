95f8cf64f5fe50b1dc5ef0adf9c3f761c054e94c
christiansallen <christiansallen@gmail.com>
2019-08-01 16:14:35 -0600

    added useContext stuff

--- firefly/src/components/MultiStepRegistration.js
+++ firefly/src/components/MultiStepRegistration.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 import RegistrationStepOne from "./RegistrationStepOne";
 import RegistrationStepTwo from "./RegistrationStepTwo";
@@ -8,77 +8,103 @@ import RegistrationStepThree from "./RegistrationStepThree";
 import CustomStepper from "./Steppers/CustomStepper";
 
 const MultiStepRegistration = () => {
-
-    const classes = makeStyles(theme => ({
-        root: {
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-        },
-        second: {
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-        },
-        third:{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-        },
-        wrapper: {
-            width:'100%'
-        }
-    }))();
-
-    const [step, setStep] = useState(0);
-    const [info, setInfo] = useState({firstName: "", lastName: "", address: "", city: "", state: "", zipCode: ""})
-    const [profiles, setProfiles] = useState([
-        {
-          firstName: "",
-          lastName: ""
-        }
-      ]);
-
-      const updateStep = (operation) => {
-        operation==='add' ? setStep(step+1) : setStep(step-1);
+  const classes = makeStyles(theme => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    second: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    third: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    wrapper: {
+      width: "100%"
     }
+  }))();
 
-    const handleInfoChanges = (e) => {
-        setInfo({...info, [e.target.name]: e.target.value});
-      }
+  const [step, setStep] = useState(0);
+  const [info, setInfo] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+    zipCode: ""
+  });
+  const [profiles, setProfiles] = useState([
+    {
+      firstName: "",
+      lastName: ""
+    }
+  ]);
+
+  const updateStep = operation => {
+    operation === "add" ? setStep(step + 1) : setStep(step - 1);
+  };
+
+  const handleInfoChanges = e => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
 
   const handleChanges = (e, i) => {
-    setProfiles(profiles.map((profile, idx) => {
-      if(idx===i){
-        return {...profile, [e.target.name]: e.target.value};
-      } else {
-        return profile;
-      }
-    }))
-  }
-
-
-
-    return (
-    step === 0 ?
-    <div className={classes.root}>
-        <h1 className="registration-header"> Step 1: Account Information</h1>
-        <CustomStepper activeStep={step}/>
-        <div className={classes.wrapper}><RegistrationStepOne step={step} updateStep={updateStep} info={info} setInfo={setInfo} handleInfoChanges={handleInfoChanges}/></div>
-    </div> : step === 1 ?
-        <div className={classes.second}>
-        <h1 className='registration-header'>Step 2: Add a Child Profile</h1>
-        <CustomStepper activeStep={step}/>
-        <div className={classes.wrapper}><RegistrationStepTwo step={step} updateStep={updateStep} profiles={profiles} setProfiles={setProfiles} handleChanges={handleChanges}/></div>
-      </div> :  <div className={classes.third}>
-      <h1 className='registration-header'>Step 3: How it Works</h1>
-      <CustomStepper activeStep={step}/>
-      <div className={classes.wrapper}><RegistrationStepThree step={step} updateStep={updateStep}/></div>
-    </div>
+    setProfiles(
+      profiles.map((profile, idx) => {
+        if (idx === i) {
+          return { ...profile, [e.target.name]: e.target.value };
+        } else {
+          return profile;
+        }
+      })
     );
   };
-  
-  export default MultiStepRegistration;
+
+  return step === 0 ? (
+    <div className={classes.root}>
+      <h1 className="registration-header"> Step 1: Account Information</h1>
+      <CustomStepper activeStep={step} />
+      <div className={classes.wrapper}>
+        <RegistrationStepOne
+          step={step}
+          updateStep={updateStep}
+          info={info}
+          setInfo={setInfo}
+          handleInfoChanges={handleInfoChanges}
+        />
+      </div>
+    </div>
+  ) : step === 1 ? (
+    <div className={classes.second}>
+      <h1 className="registration-header">Step 2: Add a Child Profile</h1>
+      <CustomStepper activeStep={step} />
+      <div className={classes.wrapper}>
+        <RegistrationStepTwo
+          step={step}
+          updateStep={updateStep}
+          profiles={profiles}
+          setProfiles={setProfiles}
+          handleChanges={handleChanges}
+        />
+      </div>
+    </div>
+  ) : (
+    <div className={classes.third}>
+      <h1 className="registration-header">Step 3: How it Works</h1>
+      <CustomStepper activeStep={step} />
+      <div className={classes.wrapper}>
+        <RegistrationStepThree step={step} updateStep={updateStep} />
+      </div>
+    </div>
+  );
+};
+
+export default MultiStepRegistration;
--- firefly/src/components/RegistrationStepOne.js
+++ firefly/src/components/RegistrationStepOne.js
@@ -5,149 +5,155 @@ import ApolloClient from "apollo-boost";
 
 import "../styles/RegistrationStepOne.scss";
 
-
-const RegistrationForm = ({ values, errors, touched, step, updateStep, info, handleInfoChanges }) => {
-  
-    return (
-      <div className="registration-container">
-        <div className="registration-forms-container">
-          <div className="registration-forms-box">
-            <Form className="registration-forms-box__formik">
-            <div className='registration-forms-two-field top-buttons'>
+const RegistrationForm = ({
+  values,
+  errors,
+  touched,
+  step,
+  updateStep,
+  info,
+  handleInfoChanges
+}) => {
+  return (
+    <div className="registration-container">
+      <div className="registration-forms-container">
+        <div className="registration-forms-box">
+          <Form className="registration-forms-box__formik">
+            <div className="registration-forms-two-field top-buttons">
               <div className="registration-forms-field-small">
                 <h2 className="registration-forms-field-title">First Name</h2>
-                <Field 
-                  type="text" 
-                  name="firstName" 
-                  className="registration-forms-box__field-small" 
+                <Field
+                  type="text"
+                  name="firstName"
+                  className="registration-forms-box__field-small"
                   value={info.firstName}
-                  onChange={(e)=>handleInfoChanges(e)}/>
+                  onChange={e => handleInfoChanges(e)}
+                />
                 {touched.firstName && errors.firstName && (
                   <p className="error">*Required</p>
                 )}
               </div>
               <div className="registration-forms-field-small">
                 <h2 className="registration-forms-field-title">Last Name</h2>
-                  <Field
-                    name="lastName"
-                    type='text'
-                    className="registration-forms-box__field-small"
-                    value={info.lastName}
-                    onChange={(e)=>handleInfoChanges(e)}
-                  />
+                <Field
+                  name="lastName"
+                  type="text"
+                  className="registration-forms-box__field-small"
+                  value={info.lastName}
+                  onChange={e => handleInfoChanges(e)}
+                />
                 {touched.lastName && errors.lastName && (
                   <p className="error">*Required</p>
                 )}
               </div>
-              </div>
-              <div className="registration-forms-field">
-                <h2 className="registration-forms-field-title-big">Address</h2>
-                  <Field
-                    name="address"
-                    className="registration-forms-box__field"
-                    type='text'
-                    value={info.address}
-                    onChange={(e)=>handleInfoChanges(e)}
-                  />
-                {touched.address && errors.address && (
-                    <p className="error-big">*Required</p>
-                  )}
-              </div>
-              <div className="registration-forms-field">
-                <h2 className="registration-forms-field-title-big">City</h2>
-                  <Field
-                    name="city"
-                    className="registration-forms-box__field"
-                    type='text'
-                    value={info.city}
-                    onChange={(e)=>handleInfoChanges(e)}
-                  />
-                {touched.city && errors.city && (
-                    <p className="error-big">*Required</p>
-                  )}
-              </div>
-              <div className='registration-forms-two-field bottom-buttons'>
+            </div>
+            <div className="registration-forms-field">
+              <h2 className="registration-forms-field-title-big">Address</h2>
+              <Field
+                name="address"
+                className="registration-forms-box__field"
+                type="text"
+                value={info.address}
+                onChange={e => handleInfoChanges(e)}
+              />
+              {touched.address && errors.address && (
+                <p className="error-big">*Required</p>
+              )}
+            </div>
+            <div className="registration-forms-field">
+              <h2 className="registration-forms-field-title-big">City</h2>
+              <Field
+                name="city"
+                className="registration-forms-box__field"
+                type="text"
+                value={info.city}
+                onChange={e => handleInfoChanges(e)}
+              />
+              {touched.city && errors.city && (
+                <p className="error-big">*Required</p>
+              )}
+            </div>
+            <div className="registration-forms-two-field bottom-buttons">
               <div className="registration-forms-field-small">
                 <h2 className="registration-forms-field-title">State</h2>
-                  <Field
-                    name="state"
-                    className="registration-forms-box__field-small"
-                    type='text'
-                    value={info.state}
-                    onChange={(e)=>handleInfoChanges(e)}
-                  />
+                <Field
+                  name="state"
+                  className="registration-forms-box__field-small"
+                  type="text"
+                  value={info.state}
+                  onChange={e => handleInfoChanges(e)}
+                />
                 {touched.state && errors.state && (
-                    <p className="error">*Required</p>
-                  )}
+                  <p className="error">*Required</p>
+                )}
               </div>
               <div className="registration-forms-field-small">
                 <h2 className="registration-forms-field-title">Zip Code</h2>
-                  <Field
-                    name="zipCode"
-                    className="registration-forms-box__field-small"
-                    type='text'
-                    value={info.zipCode}
-                    onChange={(e)=>handleInfoChanges(e)}
-                  />
+                <Field
+                  name="zipCode"
+                  className="registration-forms-box__field-small"
+                  type="text"
+                  value={info.zipCode}
+                  onChange={e => handleInfoChanges(e)}
+                />
                 {touched.zipCode && errors.zipCode && (
-                    <p className="error">*Required</p>
-                  )}
+                  <p className="error">*Required</p>
+                )}
               </div>
-              </div>
-            </Form>
-          </div>
-          <div className='registration-buttons'>
-            <button className={step===0 ? 'none' : 'registration-buttons__back'}>
-                Back
-            </button>
- 
-            <button className='registration-buttons__next' onClick={()=>updateStep('add')}>
-                Next
-            </button>
-          </div>
+            </div>
+          </Form>
+        </div>
+        <div className="registration-buttons">
+          <button
+            className={step === 0 ? "none" : "registration-buttons__back"}
+          >
+            Back
+          </button>
+
+          <button
+            className="registration-buttons__next"
+            onClick={() => updateStep("add")}
+          >
+            Next
+          </button>
         </div>
       </div>
-    );
-  };
-  
-  const RegistrationStepOne = withFormik({
-    mapPropsToValues({ firstName, lastName, address, city, state, zipCode }) {
-      return {
-        firstName: firstName || "",
-        lastName: lastName || "",
-        address: address || "",
-        city: city || "",
-        state: state || "",
-        zipCode: zipCode || ""
-      };
-    },
-  
-    validationSchema: Yup.object().shape({
-      firstName: Yup.string()
-        .required(),
-      lastName: Yup.string()
-        .required(),
-      address: Yup.string()
-        .required(),
-      city: Yup.string()
-        .required(),
-      state: Yup.string()
-        .required(),
-      zipCode: Yup.string()
-        .required(),
-    }),
-  
-    handleSubmit(values, { setSubmitting }) {
-      const client = new ApolloClient({
-        uri: "http://localhost:3300"
-      });
-      const firstName = values.firstName;
-      const lastName = values.lastName;
-      const address = values.address;
-      const city = values.city;
-      const state = values.state;
-      const zipCode = values.zipCode;
-    }
-  })(RegistrationForm);
-  
-  export default RegistrationStepOne;
+    </div>
+  );
+};
+
+const RegistrationStepOne = withFormik({
+  mapPropsToValues({ firstName, lastName, address, city, state, zipCode }) {
+    return {
+      firstName: firstName || "",
+      lastName: lastName || "",
+      address: address || "",
+      city: city || "",
+      state: state || "",
+      zipCode: zipCode || ""
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    firstName: Yup.string().required(),
+    lastName: Yup.string().required(),
+    address: Yup.string().required(),
+    city: Yup.string().required(),
+    state: Yup.string().required(),
+    zipCode: Yup.string().required()
+  }),
+
+  handleSubmit(values, { setSubmitting }) {
+    const client = new ApolloClient({
+      uri: "http://localhost:3300"
+    });
+    const firstName = values.firstName;
+    const lastName = values.lastName;
+    const address = values.address;
+    const city = values.city;
+    const state = values.state;
+    const zipCode = values.zipCode;
+  }
+})(RegistrationForm);
+
+export default RegistrationStepOne;
