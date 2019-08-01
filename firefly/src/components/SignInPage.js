1918298d000a37d017f8ef74d82f2521b2e8bb6b
charles-rogers <c.a.r0g3r5@gmail.com>
2019-08-01 18:40:42 -0500

    trying to style the firebase

--- firefly/src/components/SignInPage.js
+++ firefly/src/components/SignInPage.js
@@ -17,7 +17,6 @@ const uiConfig = {
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
   callbacks: {
     signInSuccess: currentUser => {
--- firefly/src/components/SignUpPage.js
+++ firefly/src/components/SignUpPage.js
@@ -28,7 +28,6 @@ const uiConfig = {
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
   callbacks: {
     signInSuccess: currentUser => {
