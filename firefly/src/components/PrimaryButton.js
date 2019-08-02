f410f950faed3c9a3149af9ad028a2ec7171b949
Leslie Thompson <lcrthompson@gmail.com>
2019-08-02 13:53:52 -0700

    button components created. added to my firefly page

---
+++ firefly/src/components/PrimaryButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Button.scss";
+
+const PrimaryButton = () => {
+  return (
+    <div className="primary-button-container">
+      <button>primary</button>
+    </div>
+  );
+};
+
+export default PrimaryButton;
