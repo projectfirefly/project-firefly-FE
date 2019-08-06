ed9a6dd7671a030e9f06e0e4203740dbd387c5de
christiansallen <christiansallen@gmail.com>
2019-08-06 15:08:05 -0600

    fixed some multi-step registration errors. took FORMIK OUT

--- firefly/src/components/RegistrationAddChild.js
+++ firefly/src/components/RegistrationAddChild.js
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
-import ApolloClient from "apollo-boost";
+import React from "react";
 
 import Firefly from "./../images/Logo.png";
 
@@ -13,53 +10,25 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faTimes);
 
-const { gql } = require("apollo-boost");
-
-const ADD_KID = gql`
-  mutation addKid($input: KidInput!) {
-    addKid(input: $input) {
-      id
-      first_name
-    }
-  }
-`;
-
-const AddChildAccount = ({
-  values,
-  errors,
-  touched,
+const RegistrationAddChild = ({
   clickedClose,
   idx,
   profile,
   profiles,
   handleChanges
 }) => {
-  // const handleChange = async (e, idx) => {
-  //     const newProfiles = await profiles.map((profile, i) => {
-  //         if(i === idx) {
-  //             return {
-  //                 ...profile,
-  //                 [e.target.name]: e.target.value
-  //             }
-  //         }
-  //         return profile
-  //     })
-  //     setProfiles(newProfiles)
-  // }
-  // console.log(profiles)
-
   const handleChange = e => {
     handleChanges(e, idx);
   };
 
   return (
-    <div className="registration-forms-box">
+    <div className="registration-forms-box-2">
       <FontAwesomeIcon
         icon="times"
         className={profiles.length === 1 ? "none" : "close-icon"}
         onClick={() => clickedClose(idx)}
       />
-      <Form className="registration-forms-box__formik">
+      <form className="registration-forms-box__formik">
         <div className="registration-forms-field-container">
           <div className="registration-firefly-container">
             <img
@@ -71,71 +40,35 @@ const AddChildAccount = ({
           <div className="registration-forms-field-two">
             <div className="registration-forms-field-two__field">
               <h2 className="registration-forms-field-title-two">First Name</h2>
-              <Field
+              <input
                 type="text"
                 name="first_name"
                 className="registration-forms-box__field"
                 value={profile.first_name}
                 onChange={e => handleChange(e)}
               />
-              {touched.first_name && errors.first_name && (
-                <p className="error-step2">*Required</p>
-              )}
+              <p className={profile.first_name === "" ? "error-step2" : "none"}>
+                *Required
+              </p>
             </div>
             <div className="registration-forms-field-two__field">
               <h2 className="registration-forms-field-title-two">Last Name</h2>
-              <Field
+              <input
                 type="text"
                 name="last_name"
                 value={profile.last_name}
                 onChange={e => handleChange(e)}
                 className="registration-forms-box__field"
               />
-              {touched.last_name && errors.last_name && (
-                <p className="error-step2">*Required</p>
-              )}
+              <p className={profile.last_name === "" ? "error-step2" : "none"}>
+                *Required
+              </p>
             </div>
           </div>
         </div>
-      </Form>
+      </form>
     </div>
   );
 };
 
-const AddChildProfile = withFormik({
-  mapPropsToValues({ first_name, last_name }) {
-    return {
-      first_name: first_name || "",
-      last_name: last_name || ""
-    };
-  },
-
-  validationSchema: Yup.object().shape({
-    first_name: Yup.string().required(),
-    last_name: Yup.string().required()
-  }),
-
-  handleSubmit(values) {
-    const client = new ApolloClient({
-      uri: "http://localhost:3300"
-    });
-    client
-      .mutate({
-        mutation: ADD_KID,
-        variables: {
-          input: {
-            first_name: values.first_name,
-            last_name: values.last_name
-          }
-        }
-      })
-      .then(res => {
-        console.log(res);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-})(AddChildAccount);
-
-export default AddChildProfile;
+export default RegistrationAddChild;
