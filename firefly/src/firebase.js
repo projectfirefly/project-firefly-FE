7de3a9fb52479f919b1a96479c27d85955fb9613
Robert Driskell <driskell.ba@gmail.com>
2019-08-06 17:14:23 -0500

    environment variables set up

--- firefly/src/firebase.js
+++ firefly/src/firebase.js
@@ -1,15 +1,18 @@
 import firebase from "firebase";
 
 // Your web app's Firebase configuration
+
+const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+
 const firebaseConfig = {
     //Don't worry about the "apiKey" being public, it gives you no power
-    apiKey: "AIzaSyACMmtTos8CBls03uYsnOxUfP3jYFqSML0",
-    authDomain: "firefly-firebase-test.firebaseapp.com",
-    databaseURL: "https://firefly-firebase-test.firebaseio.com",
-    projectId: "firefly-firebase-test",
-    storageBucket: "firefly-firebase-test.appspot.com",
-    messagingSenderId: "881673137177",
-    appId: "1:881673137177:web:361a0bbeb3336d54",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: `${projectId}.firebaseapp.com`,
+    databaseURL: `https://${projectId}.firebaseio.com`,
+    projectId: `${projectId}`,
+    storageBucket: `${projectId}.appspot.com`,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
