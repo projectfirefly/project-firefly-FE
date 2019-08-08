0fbcf32508b003655a51a800435dfa9229374f7a
Leslie Thompson <lcrthompson@gmail.com>
2019-08-08 10:21:50 -0700

    starting to work in ffbox

--- firefly/src/components/game/FFbox.js
+++ firefly/src/components/game/FFbox.js
@@ -1,7 +1,18 @@
 import React from "react";
+import AnimationTest from "../part2/AnimationTest";
 
 const FFbox = () => {
-  return <div />;
+  return (
+    <div>
+      <AnimationTest
+        height={250}
+        width={250}
+        accessory="nerdglasses"
+        color={352}
+      />
+      <p>where you at??</p>
+    </div>
+  );
 };
 
 export default FFbox;
--- firefly/src/components/game/GameBoard.js
+++ firefly/src/components/game/GameBoard.js
@@ -1,11 +1,17 @@
 import React from "react";
 import FFbox from "./FFbox";
 import BlockLine from "./BlockLine";
+import styled from "styled-components";
+
+const Styled = styled(FFbox)`
+  border: 2px solid green;
+  height: 200px;
+`;
 
 const GameBoard = () => {
   return (
     <div>
-      <FFbox />
+      <Styled />
       <BlockLine />
     </div>
   );
