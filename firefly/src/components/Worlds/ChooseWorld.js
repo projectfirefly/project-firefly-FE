dc07ea287c3900553fd0628607c3483775eb4660
Leslie Thompson <lcrthompson@gmail.com>
2019-08-14 14:42:06 -0700

    basic styling on choose world added. made responsive, added WorldCard component

--- firefly/src/components/Worlds/ChooseWorld.js
+++ firefly/src/components/Worlds/ChooseWorld.js
@@ -1,11 +1,20 @@
 import React from "react";
 import chooseWorldStyles from "./ChooseWorldStyles";
+import WorldCard from "./WorldCard";
+import fireflyWorld1 from "./fireflyWorld1.png";
+import lockedWorld from "./lockedWorld.png";
 
 const ChooseWorld = () => {
   const classes = chooseWorldStyles();
   return (
     <div className={classes.rootContainer}>
       <hl className={classes.title}> CHOOSE YOUR WORLD </hl>
+      <div className={classes.worldContainer}>
+        <WorldCard title={"Fireflies"} img={fireflyWorld1} />
+        <WorldCard title={"Coming Soon"} img={lockedWorld} />
+        <WorldCard title={"Coming Soon"} img={lockedWorld} />
+        <WorldCard title={"Coming Soon"} img={lockedWorld} />
+      </div>
     </div>
   );
 };
--- firefly/src/components/Worlds/ChooseWorldStyles.js
+++ firefly/src/components/Worlds/ChooseWorldStyles.js
@@ -2,20 +2,50 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const chooseWorldStyles = makeStyles({
   rootContainer: {
+    marginTop: "10%",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    // marginTop: "10%",
     width: "100%",
     fontFamily: "'Nunito', sans-serif"
   },
   title: {
-    fontSize: "34pt",
+    fontSize: "34px",
     fontWeight: "900",
-    letterSpacing: "7pt",
-    lineHeight: "46pt",
-    color: "#5b4eff"
+    letterSpacing: "7px",
+    lineHeight: "46px",
+    color: "#5b4eff",
+    marginBottom: "30px"
+  },
+  worldContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    // border: "solid pink 1px",
+    width: "80%",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  ///////// World Card Styling //////
+  worldCard: {
+    width: "370px",
+    height: "226px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "20px",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",
+    backgroundColor: "white",
+    marginBottom: "50px",
+    h4: {
+      fontSize: "20px",
+      color: "#152F04",
+      fontWeight: "bold"
+    }
+  },
+  worldCardImg: {
+    width: "50%"
   }
 });
 
