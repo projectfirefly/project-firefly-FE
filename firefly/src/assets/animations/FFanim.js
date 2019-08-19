a5b3fe831880445bdd72409e30a40095da66bb5c
Robert Driskell <driskell.ba@gmail.com>
2019-08-19 16:36:32 -0500

    Fixed slider lag again

--- firefly/src/assets/animations/FFanim.js
+++ firefly/src/assets/animations/FFanim.js
@@ -3,6 +3,7 @@ import Lottie from "react-lottie";
 import awakeanimationData from "./ffAnim.json";
 import sleepingAnimation from "./sleeping.json";
 import styled from "styled-components";
+import { makeStyles } from "@material-ui/styles";
 
 const StyledLottie = styled.div.attrs(props => ({}))`
   .lambdahat {
@@ -46,6 +47,61 @@ const StyledLottie = styled.div.attrs(props => ({}))`
 `;
 
 const FFanim = ({ height, width, color, accessory, awake }) => {
+  function accessorySwitch( selector ) {
+    if (accessory === selector) {
+      return "block !important"
+    } else {
+      return "none !important"
+    }
+  }
+
+  const classes = makeStyles(theme => ({
+    wrapper: {
+      "& .lambdahat": {
+        display: `${accessorySwitch("lambdahat")}`,
+      },
+      "& .sunglasses": {
+        display: `${accessorySwitch("sunglasses")}`,
+      },
+      "& .headphones": {
+        display: `${accessorySwitch("headphones")}`,
+      },
+      "& .nerdglasses": {
+        display: `${accessorySwitch("nerdglasses")}`,
+      },
+      "& .bodyLightTop": {
+        "& path": {
+          fill: `hsl(${color}, 100%, 30%)`,
+        },
+      },
+      "& .bodyLightMid": {
+        "& path": {
+          fill: `hsl(${color}, 100%, 40%)`,
+        },
+      },
+      "& .bodyLightBottom": {
+        "& path": {
+          fill: `hsl(${color}, 100%, 55%)`,
+        },
+      },
+      "& .lightL": {
+        "& path": {
+          stroke: `hsl(${color}, 100%, 55%)`,
+        },
+      },
+      "& .lightM": {
+        "& path": {
+          stroke: `hsl(${color}, 100%, 55%)`,
+        },
+      },
+      "& .lightR": {
+        "& path": {
+          stroke: `hsl(${color}, 100%, 55%)`,
+        },
+      },
+    },
+  }))();
+
   let animationData2 = {};
   if (awake) {
     animationData2 = awakeanimationData;
@@ -57,14 +113,16 @@ const FFanim = ({ height, width, color, accessory, awake }) => {
     autoplay: true,
     animationData: animationData2,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
 
   return (
-    <StyledLottie accessory={accessory} color={color}>
+    // <StyledLottie accessory={accessory} color={color}>
+    <div className={classes.wrapper}>
       <Lottie options={defaultOptions} height={height} width={width} />
-    </StyledLottie>
+    </div>
+    // </StyledLottie>
   );
 };
 
--- firefly/src/components/ColorSlider/ColorSlider.jsx
+++ firefly/src/components/ColorSlider/ColorSlider.jsx
@@ -4,77 +4,77 @@ import Slider from "@material-ui/core/Slider";
 
 export default function ColorSlider(props) {
 
-    const [sliderValue, setSliderValue] = useState();
+  const [sliderValue, setSliderValue] = useState();
 
-    useEffect(() => {
-        setSliderValue(props.value);
-    }, [props])
+  useEffect(() => {
+    setSliderValue(props.value);
+  }, [props])
 
-    const useStyles = makeStyles({
-        root: {
-            color: `hsl(${sliderValue},100%,50%)`,
-            width: '100%',
-        },
-        track: {
-            height: 11,
-            borderRadius: 5,
-            background: 'rgb(255,255,255)',
-            opacity: 0,
-        },
-        rail: {
-            height: 11,
-            borderRadius: 5,
-            background: `linear-gradient(to right, hsl(0,70%,50%),
+  const useStyles = makeStyles({
+    root: {
+      color: `hsl(${sliderValue},100%,50%)`,
+      width: '100%',
+    },
+    track: {
+      height: 11,
+      borderRadius: 5,
+      background: 'rgb(255,255,255)',
+      opacity: 0,
+    },
+    rail: {
+      height: 11,
+      borderRadius: 5,
+      background: `linear-gradient(to right, hsl(0,70%,50%),
             hsl(${(360) / 5},100%,50%),
             hsl(${(360 * 2) / 5},100%,50%),
             hsl(${(360 * 3) / 5},100%,50%),
             hsl(${(360 * 4) / 5},100%,50%),
             hsl(${360},100%,50%))`,
-            opacity: '1',
-        },
-        thumb: {
-            height: 27,
-            width: 27,
-            backgroundColor: '#EEE',
-            border: '4px solid currentColor',
-            marginTop: -8,
-            marginLeft: -12,
-            '&:focus,&:hover,&:active': {
-                boxShadow: 'inherit',
-            },
-        },
-        active: {
-            height: 48,
-            width: 48,
-            marginTop: -18,
-            marginLeft: -24,
-            border: '8px solid currentColor',
-        },
-    })
+      opacity: '1',
+    },
+    thumb: {
+      height: 27,
+      width: 27,
+      backgroundColor: '#EEE',
+      border: '4px solid currentColor',
+      marginTop: -8,
+      marginLeft: -12,
+      '&:focus,&:hover,&:active': {
+        boxShadow: 'inherit',
+      },
+    },
+    active: {
+      height: 48,
+      width: 48,
+      marginTop: -18,
+      marginLeft: -24,
+      border: '8px solid currentColor',
+    },
+  })
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const change = (event, newValue) => {
-      setSliderValue(newValue);
-      const d = new Date();
-      if (d.getTime() % 7 === 0) {
-        props.updateColor(newValue);
-      }
+  const change = (event, newValue) => {
+    const d = new Date();
+    if (d.getTime() % 3 === 0) {
+      props.updateColor(newValue);
     }
+  }
 
-    const commitChange = (event, newValue) => {
-        props.updateColor(newValue);
-    }
+  const commitChange = (event, newValue) => {
+    setSliderValue(newValue);
+    props.updateColor(newValue);
+  }
 
-    return (
-        <div style={{ width: '100%', height:'100%' }}>
-            <div>
-                {sliderValue > -1 ?
-                    <Slider classes={classes} value={sliderValue} max={360} onChange={change} onChangeCommitted={commitChange} aria-label="Color slider" />
-                    :
-                    <h2>loading...</h2>
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <div>
+        {sliderValue > -1 ?
+          <Slider step={1} classes={classes} value={sliderValue} max={360} onChange={change} onChangeCommitted={commitChange} aria-label="Color slider" />
+          :
+          <h2>loading...</h2>
+        }
+      </div>
+    </div>
+  );
 }
