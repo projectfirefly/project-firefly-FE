b98971896a6f714ddb0f6be977f7364a93edba34
christiansallen <christiansallen@gmail.com>
2019-08-19 11:00:49 -0700

    made firefly box background conditional

--- firefly/src/components/game/FFbox.js
+++ firefly/src/components/game/FFbox.js
@@ -1,24 +1,35 @@
 import React from "react";
 // import AnimationTest from "../part2/AnimationTest";
 import FFanim from "../../assets/animations/FFanim";
-import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
 
-const FireFlyBox = styled.div`
-  width: 40%;
-  padding: 20px 0;
-  margin: 5% auto;
-  border-radius: 20px;
+const useStyles = makeStyles({
+  fireFlyBox: {
+    width: "40%",
+    padding: "20px 0",
+    margin: "5% auto",
+    borderRadius: "20px",
+    boxShadow: "0px 2px 4px #000000",
+    backgroundImage: "linear-gradient(#382eb8, #7068ff)"
+  },
 
-  background-image: linear-gradient(#555, #fff);
-  /* background-image: linear-gradient(#382eb8, #7068ff); */
-  box-shadow: 0px 2px 4px #000000;
-`;
+  fireFlyBoxSleep: {
+    width: "40%",
+    padding: "20px 0",
+    margin: "5% auto",
+    borderRadius: "20px",
+    boxShadow: "0px 1px 4px #000000",
+    backgroundImage: "linear-gradient(#555, #fff)"
+  }
+});
 
 const FFbox = ({ tools }) => {
+  const classes = useStyles();
   return (
-    <FireFlyBox
-    //Conditionally render the FF box depending on play AND led blocks are on. We don't need it anymore.
-    // style={!tools[0].used || !tools[1].used ? { visibility: "hidden" } : null}
+    <div
+      className={tools[1].used ? classes.fireFlyBox : classes.fireFlyBoxSleep}
+      //Conditionally render the FF box depending on play AND led blocks are on. We don't need it anymore.
+      // style={!tools[0].used || !tools[1].used ? { visibility: "hidden" } : null}
     >
       <FFanim
         height={250}
@@ -27,7 +38,7 @@ const FFbox = ({ tools }) => {
         color={58}
         awake={tools[1].used ? true : false}
       />
-    </FireFlyBox>
+    </div>
   );
 };
 
