b4309d90a6580fdd4b537d5a569b3703a8ca74b4
Leslie Thompson <lcrthompson@gmail.com>
2019-08-21 10:48:04 -0700

    world FF menu styling. working on adding a deleting world FF

--- firefly/src/components/Worlds/WorldFirefly/WorldFirefly.js
+++ firefly/src/components/Worlds/WorldFirefly/WorldFirefly.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  removeFirefly,
+  addFirefly
+} from "../../../utils/firebaseInteractions.jsx";
 
 import FFanim from "../../../assets/animations/FFanim";
+import FFicon from "../../../assets/icons/firefly/Firefly";
 import WorldFireflyStyles from "./WorldFireflyStyles";
-import { FaPen, FaTrashAlt, FaArrowsAlt } from "react-icons/fa";
+import {
+  FaPen,
+  FaTrashAlt,
+  FaArrowsAlt,
+  FaTimes,
+  FaCheck
+} from "react-icons/fa";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Typography
+} from "@material-ui/core";
+import { ADD_FIREFLY, REMOVE_FIREFLY } from "../../../context/Game/GameStore";
 
-const WorldFirefly = () => {
+const WorldFirefly = props => {
   const [menuActive, setMenuState] = useState(false);
-  const [trashActive, setTrashState] = useState(false);
+  const [trashOpen, setTrashOpen] = useState(false);
+
+  const confirmRemove = () => {
+    removeFirefly(REMOVE_FIREFLY).then(() => {
+      props.history.push("/choose-profile");
+    });
+  };
 
   const classes = WorldFireflyStyles();
 
@@ -18,15 +44,10 @@ const WorldFirefly = () => {
         <Link to="/game">
           <FaPen className={classes.pen} />
         </Link>
-        <div onClick={() => setTrashState(!trashActive)}>
+        <div onClick={() => setTrashOpen(true)}>
           <FaTrashAlt className={classes.trash} />
         </div>
       </div>
-      {/* Delete Session */}
-      <div className={classes.trashAlert}>
-        <div className={classes.trashAlertBox}>{/* <h2> BYE BYE!</h2> */}</div>
-      </div>
-
       <div onClick={() => setMenuState(!menuActive)}>
         <FFanim
           height={129}
@@ -36,6 +57,42 @@ const WorldFirefly = () => {
           awake={true}
         />
       </div>
+      <Dialog
+        open={trashOpen}
+        onClose={() => setTrashOpen(false)}
+        are-labelledby="remove-profile-dialog"
+        // className={classes.dialog}
+        classes={{
+          paper: classes.dialogPaper
+        }}
+      >
+        <DialogContent className={classes.dialogContainer}>
+          <div className={classes.dialogTop}>
+            <FFicon height={257} width={264} accessory="none" color={642} />
+            <DialogContentText className={classes.dialogText}>
+              BYE BYE!
+            </DialogContentText>
+          </div>
+
+          <DialogActions>
+            <div className={classes.dialogButtonContainer}>
+              <button
+                className={classes.dialogButtons + " cancel"}
+                onClick={() => setTrashOpen(false)}
+              >
+                <FaTimes />
+              </button>
+
+              <button
+                onClick={confirmRemove}
+                className={classes.dialogButtons + " remove"}
+              >
+                <FaCheck />
+              </button>
+            </div>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
