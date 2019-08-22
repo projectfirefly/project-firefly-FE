3fd4885c5c90fce3490a666b9a292be2efb0c725
Robert Driskell <driskell.ba@gmail.com>
2019-08-22 18:25:26 -0500

    Fixed a couple small bugs related to profiles

--- firefly/src/components/ChildProfiles/ChooseProfilePage.jsx
+++ firefly/src/components/ChildProfiles/ChooseProfilePage.jsx
@@ -35,48 +35,54 @@ const ChooseProfilePage = props => {
 
   const classes = chooseProfileStyles();
 
-  return (
-    <div className={classes.root}>
-      <Typography variant="h1" className={classes.header}>
-        {" "}
-        CHOOSE YOUR FIREFLY{" "}
-      </Typography>
-      <div className={classes.sizingContainer}>
-        <div className={classes.cardContainer}>
-          {childProfileState.user.profiles.map(profile => (
-            <div
-              className={classes.card}
-              onClick={() => {
-                dispatch({
-                  type: "UPDATE_SELECTED",
-                  payload: profile.id
-                });
-                gettingWorld(profile);
-                props.history.push("/myfirefly");
-              }}
-            >
-              <div className={classes.text}>
-                <Typography variant="h4" className={classes.name}>
-                  {profile.first_name}
-                </Typography>
+  if (childProfileState.user.profiles) {
+    return (
+      <div className={classes.root}>
+        <Typography variant="h1" className={classes.header}>
+          {" "}
+          CHOOSE YOUR FIREFLY{" "}
+        </Typography>
+        <div className={classes.sizingContainer}>
+          <div className={classes.cardContainer}>
+            {childProfileState.user.profiles.map(profile => (
+              <div
+                className={classes.card}
+                onClick={() => {
+                  dispatch({
+                    type: "UPDATE_SELECTED",
+                    payload: profile.id
+                  });
+                  gettingWorld(profile);
+                  props.history.push("/myfirefly");
+                }}
+              >
+                <div className={classes.text}>
+                  <Typography variant="h4" className={classes.name}>
+                    {profile.first_name}
+                  </Typography>
+                </div>
+                <div className={classes.flysize}>
+                  <ProfileFly
+                    color={profile.avatar.color}
+                    accessory={profile.avatar.accessory}
+                  />
+                </div>
               </div>
-              <div className={classes.flysize}>
-                <ProfileFly
-                  color={profile.avatar.color}
-                  accessory={profile.avatar.accessory}
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className={classes.backButtonContainer}>
-          <Link to="/startgame" className={classes.backButtonStyle}>
-            <Typography variant="button">Back</Typography>
-          </Link>
+            ))}
+          </div>
+          <div className={classes.backButtonContainer}>
+            <Link to="/startgame" className={classes.backButtonStyle}>
+              <Typography variant="button">Back</Typography>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div></div>
+    )
+  }
 };
 
 export default ChooseProfilePage;
