0e8ed36c492b47d80d52f751b5109bf7b70c8e4e
Leslie Thompson <lcrthompson@gmail.com>
2019-08-23 15:10:37 -0700

    remove border for merge

--- firefly/src/components/Worlds/FireflyWorld/FireflyWorldStyles.js
+++ firefly/src/components/Worlds/FireflyWorld/FireflyWorldStyles.js
@@ -21,7 +21,7 @@ const chooseWorldStyles = makeStyles(theme => ({
     fontFamily: "'Nunito', sans-serif"
   },
   fireflyContainer: {
-    border: "dashed yellow 2px",
+    // border: "dashed yellow 2px",
     marginTop: "10%",
     width: "90%",
     position: "absolute",
