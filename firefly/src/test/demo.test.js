9e71e878ff709b76758aa08e9ebb71e7241206ed
aaharbaugh <aaronmharbaugh@gmail.com>
2019-08-27 15:37:34 -0500

    random non-working tests

--- firefly/src/test/demo.test.js
+++ firefly/src/test/demo.test.js
@@ -1,10 +1,14 @@
-import React, { useContext, useState, useEffect } from "react";
-import MyFireflyPage from '../components/childProfile/MyFireflyPage';
-import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+import ReactDOM from "react-dom";
+import RegistrationAddChild from "../components/Registration/RegistrationAddChild";
+import App from "../App";
 
+const profiles = {
+    length: 1
+}
 
-
-it("render myFireflyPage <Component>", () => {
-    expect(1+2).toBe(3);
- });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
