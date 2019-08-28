90f1f7a65ccba64a5673e4288317d1b0a11cab97
Robert Driskell <driskell.ba@gmail.com>
2019-08-28 14:44:49 -0500

    moved more stuff around

--- firefly/src/components/Worlds/WorldNav/WorldNavStyles.js
+++ firefly/src/components/Worlds/WorldNav/WorldNavStyles.js
@@ -1,51 +1,41 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const WorldNavStyles = makeStyles({
+const WorldNavStyles = makeStyles(theme => ({
   navContainer: {
     display: "flex",
-    width: "100%",
+    width: "90%",
     // border: "white 6px solid",
-    justifyContent: "space-between",
-    marginTop: "50px"
-  },
-
-  iconButton: {
-    display: "flex",
-    width: "44px",
-    height: "44px",
-    fontSize: "20px",
-    textAlign: "right",
     alignItems: "center",
-    justifyContent: "center",
-    color: " #4aa810",
-    fontWeight: "bold",
-    fontSize: "18px",
-    border: " 2px solid #abb0ba",
-    borderRadius: " 10px",
-    backgroundColor: " #ffffff",
-    boxShadow: "0px 3px #8f96a3",
-    marginLeft: "50px"
+    justifyContent: "space-between",
+    marginTop: "5%",
+  },
+  x: {
+    lineHeight: "0",
+    fontSize: "2rem",
+  },
+  iconButton: {
+    ...theme.secondaryButton,
+    padding: "1.2rem",
+    marginBottom: "1rem",
   },
 
   addButton: {
-    display: "flex",
-    width: "113px",
-    height: "85px",
-    fontSize: "36px",
-    lineHeight: "37px",
-    textAlign: "right",
+    ...theme.secondaryButton,
+    padding: "1.6rem",
     alignItems: "center",
-    justifyContent: "center",
-    color: " #4aa810",
-    fontWeight: "bold",
-    fontSize: "18px",
-    border: " 2px solid #abb0ba",
-    borderRadius: " 10px",
-    backgroundColor: " #ffffff",
-    boxShadow: "0px 3px #8f96a3",
-    marginRight: "50px"
   },
-  link: { textDecoration: "none", color: " #4aa810" }
-});
+  link: {
+    textDecoration: "none",
+    color: " #4aa810",
+  },
+  ffOutline: {
+    width: "5rem",
+    marginLeft: "1rem",
+  },
+  plus: {
+    fontSize: "3rem",
+    lineHeight: "0",
+  },
+}));
 
 export default WorldNavStyles;
