6ed039e5a6f605ae43a4b074af16fd57c7acf540
Andrew Benedict <atbenedict@gmail.com>
2019-08-29 11:47:45 -0400

    made chilling fly unclickable

--- firefly/src/assets/animations/ChillingFlyNoAcc.js
+++ firefly/src/assets/animations/ChillingFlyNoAcc.js
@@ -6,14 +6,21 @@ const ChillingFlyNoAcc = ({ height = 160, width = 160 }) => {
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    isClickToPauseDisabled: true,
+    // isClickToPauseDisabled: true,
     animationData: animationData,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice"
     }
   };
 
-  return <Lottie options={defaultOptions} height={height} width={width} />;
+  return (
+    <Lottie
+      options={defaultOptions}
+      height={height}
+      width={width}
+      isClickToPauseDisabled={true}
+    />
+  );
 };
 
 export default ChillingFlyNoAcc;
