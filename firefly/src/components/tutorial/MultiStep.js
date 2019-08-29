216a91605d4f5741aaf4c9890deacd7bdd98c8c1
aaharbaugh <aaronmharbaugh@gmail.com>
2019-08-29 14:04:23 -0500

    fixed fuctionality, realigned images and arrows, added buckbutton

--- firefly/src/components/tutorial/MultiStep.js
+++ firefly/src/components/tutorial/MultiStep.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
 import screen1 from "../../assets/images/screen1.png";
 import screen2 from "../../assets/images/screen2.png";
@@ -13,7 +14,7 @@ import screen7 from "../../assets/images/screen7.png";
 import leftarrow from "../../assets/images/BlueLeftArrow.svg";
 import rightarrow from "../../assets/images/BlueRightArrow.svg";
 
-const MultiStepTutorial = () => {
+const MultiStepTutorial = props => {
   const classes = makeStyles(theme => ({
     app: {},
     root: {
@@ -25,7 +26,8 @@ const MultiStepTutorial = () => {
       backgroundColor: "#D0CCFF"
     },
     image: {
-      width: "100vw"
+      height: "100vh",
+      marginLeft: "-55px"
     },
     wrapper: {
       height: "100%",
@@ -39,19 +41,56 @@ const MultiStepTutorial = () => {
       display: "flex",
       justifyContent: "space-between",
       width: "100px",
-      marginLeft: "31px"
+      marginLeft: "1px"
     },
     disabled: {
       opacity: "0.5"
+    },
+    topBar: {
+      position: "absolute",
+      left: "5%",
+      top: "0",
+      display: "flex",
+      justifyContent: "space-between",
+      margin: "0px 1%",
+      marginTop: "2%",
+      width: "90%"
+    },
+    topBarButton: {
+      ...theme.secondaryButton,
+      display: "flex",
+      color: "#4aa810",
+      cursor: "pointer",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "1.8rem",
+      width: "3.4rem",
+      height: "3.4rem",
+      textDecoration: "none"
     }
   }))();
 
   const [step, setStep] = useState(0);
+  const [back, setBack] = useState(0);
+
+  useEffect(() => {
+    if (back === 1) {
+      props.history.goBack();
+    }
+  }, [back]);
 
   switch (step) {
     default:
       return (
         <div className={classes.root}>
+          <div className={classes.topBar}>
+            <div className={classes.topBarButton}>
+              <i
+                className="fas fa-sign-out-alt"
+                onClick={() => setBack(back + 1)}
+              />
+            </div>
+          </div>
           <div className={classes.wrapper}>
             <div className={classes.arrowcontainer}>
               <img src={leftarrow} className={classes.disabled} />
@@ -69,10 +108,7 @@ const MultiStepTutorial = () => {
               <img src={leftarrow} onClick={() => setStep(step - 1)} />
               <img src={rightarrow} onClick={() => setStep(step + 1)} />
             </div>
-            <img
-              className={classes.image}
-              src={screen2}
-            />
+            <img className={classes.image} src={screen2} />
           </div>
         </div>
       );
@@ -84,10 +120,7 @@ const MultiStepTutorial = () => {
               <img src={leftarrow} onClick={() => setStep(step - 1)} />
               <img src={rightarrow} onClick={() => setStep(step + 1)} />
             </div>
-            <img
-              className={classes.image}
-              src={screen3}
-            />
+            <img className={classes.image} src={screen3} />
           </div>
         </div>
       );
@@ -99,10 +132,7 @@ const MultiStepTutorial = () => {
               <img src={leftarrow} onClick={() => setStep(step - 1)} />
               <img src={rightarrow} onClick={() => setStep(step + 1)} />
             </div>
-            <img
-              className={classes.image}
-              src={screen4}
-            />
+            <img className={classes.image} src={screen4} />
           </div>
         </div>
       );
@@ -112,45 +142,36 @@ const MultiStepTutorial = () => {
           <div className={classes.wrapper}>
             <div className={classes.arrowcontainer}>
               <img src={leftarrow} onClick={() => setStep(step - 1)} />
-              <img src={rightarrow} onClick={() => setStep(step + 1)}/>
+              <img src={rightarrow} onClick={() => setStep(step + 1)} />
             </div>
-            <img
-              className={classes.image}
-              src={screen5}
-            />
+            <img className={classes.image} src={screen5} />
           </div>
         </div>
       );
-      case 5:
+    case 5:
       return (
         <div className={classes.root}>
           <div className={classes.wrapper}>
             <div className={classes.arrowcontainer}>
               <img src={leftarrow} onClick={() => setStep(step - 1)} />
-              <img src={rightarrow} onClick={() => setStep(step + 1)}/>
+              <img src={rightarrow} onClick={() => setStep(step + 1)} />
             </div>
-            <img
-              className={classes.image}
-              src={screen6}
-            />
+            <img className={classes.image} src={screen6} />
           </div>
         </div>
       );
-      case 6:
-        return (
-          <div className={classes.root}>
-            <div className={classes.wrapper}>
-              <div className={classes.arrowcontainer}>
-                <img src={leftarrow} onClick={() => setStep(step - 1)} />
-                <img src={rightarrow} className={classes.disabled}/>
-              </div>
-              <img
-                className={classes.image}
-                src={screen7}
-              />
+    case 6:
+      return (
+        <div className={classes.root}>
+          <div className={classes.wrapper}>
+            <div className={classes.arrowcontainer}>
+              <img src={leftarrow} onClick={() => setStep(step - 1)} />
+              <img src={rightarrow} className={classes.disabled} />
             </div>
+            <img className={classes.image} src={screen7} />
           </div>
-        );
+        </div>
+      );
   }
 };
 
