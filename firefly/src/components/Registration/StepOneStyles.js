388545818d81f55a970491fb38153099e225154b
charles-rogers <c.a.r0g3r5@gmail.com>
2019-08-29 21:37:02 -0500

    mobile design

--- firefly/src/components/Registration/StepOneStyles.js
+++ firefly/src/components/Registration/StepOneStyles.js
@@ -1,11 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const stepOneStyles = makeStyles(theme => ({
-  mainContainer: {
-    width:"98%",
-
-
-  },
   container: {
     display: "flex",
     flexDirection: "column",
@@ -35,29 +30,48 @@ const stepOneStyles = makeStyles(theme => ({
     flexDirection: "column",
     alignItems: "center",
     width: "100%",
+    height:'100%',
     padding: "2rem 3rem",
     marginTop: "1rem",
     borderRadius: "20px",
     backgroundColor: "white",
     boxShadow: " 0px 20px 30px -20px rgba(0, 0, 0, 0.3)",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      marginBottom: '3%',
+  },
   },
   smallInputContainer: {
     display: "flex",
     width: "100%",
     justifyContent: "space-around",
     alignItems: "space-around",
-    margin: ".5rem 0"
+    margin: ".5rem 0",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      display: "flex",
+      flexDirection: "column",
+    },
   },
   smallInputSizer: {
     display: "flex",
     flexDirection: "column",
-    width: "45%"
+    width: "45%",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      width: "95%"
+    },
   },
   smallInput: {
     ...theme.input,
     width: "90%",
     margin: "1rem .2rem",
-    padding: "1rem 1rem"
+    padding: "1rem 1rem",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      width: "95.5%",
+      margin: "1rem 0",
+    },
   },
   bigInputContainer: {
     display: "flex",
@@ -69,7 +83,12 @@ const stepOneStyles = makeStyles(theme => ({
   bigInputSizer: {
     display: "flex",
     width: "95%",
-    flexDirection: "column"
+    flexDirection: "column",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      width:'96%',
+
+    },
   },
   bigInput: {
     ...theme.input,
@@ -85,35 +104,78 @@ const stepOneStyles = makeStyles(theme => ({
     justifyContent: "center",
     width: "100%",
     marginBottom: "3rem",
-    height: "50px"
+    height: "50px",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      display: "flex",
+      flexDirection: "column-reverse",
+      alignItems: 'center',
+      width:'100%',
+      height:'100%',
+      marginBottom: '0',
+
+    },
   },
   buttonContainer2: {
     display: "flex",
     justifyContent: "center",
     width: "100%",
     marginTop: "3rem",
-    marginBottom: "4rem"
+    marginBottom: "4rem",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      display: "flex",
+      flexDirection: "column-reverse",
+      alignItems: 'center',
+      width:'80%',
+      margin: '15px auto',
+  },
   },
   nextButtonDisabled: {
     ...theme.disabledButton,
     padding: ".7rem 5rem",
-    width: "30%"
+    width: "30%",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      marginBottom: '10%',
+      marginRight: '0',
+      width:'100%',
+
+    },
   },
   nextButton: {
     ...theme.primaryButton,
     padding: ".7rem 5rem",
-    width: "30%"
+    width: "30%",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      marginBottom: '10%',
+      marginRight: '0',
+      width:'100%',
+
+    },
   },
   nextButton3: {
     ...theme.primaryButton,
     padding: ".7rem 5rem",
-    width: "40%"
+    width: "40%",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      width:'100%',
+      marginBottom: '10%',
+  },
   },
   backButton: {
     ...theme.secondaryButton,
     padding: ".7rem 5rem",
     width: "30%",
-    marginRight: "40px"
+    marginRight: "40px",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      marginRight: '0',
+      width:'100%',
+
+    },
   },
   link: { color: "#4BA810", textDecoration: "none" },
   link3: { color: "white", textDecoration: "none" },
@@ -121,7 +183,13 @@ const stepOneStyles = makeStyles(theme => ({
     ...theme.secondaryButton,
     padding: ".7rem 5rem",
     width: "40%",
-    marginRight: "40px"
+    marginRight: "40px",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      width:'100%',
+      marginBottom: '5%',
+      marginRight:'0',
+  },
   },
   addChild: {
     width: "100%",
@@ -135,6 +203,10 @@ const stepOneStyles = makeStyles(theme => ({
     overflowY: "scroll",
     zIndex: "10",
     marginBottom: "3rem",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      marginBottom: '0',
+  },
 
     "&::-webkit-scrollbar": {
       width: "1%"
@@ -148,20 +220,31 @@ const stepOneStyles = makeStyles(theme => ({
     margin: "0 auto",
     display: "flex",
     flexDirection: "column",
-    width: "70%"
+    width: "70%",
   },
   stepThreeContainer: {
     margin: "0 auto",
     borderRadius: "20px",
     backgroundColor: "white",
     boxShadow: " 0px 20px 30px -20px rgba(0, 0, 0, 0.3)",
-    width: "85%"
+    width: "85%",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      marginBottom: '5%',
+  },
   },
   boxes: {
     margin: "0 auto",
     display: "flex",
     width: "85%",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: 'center',
+      marginBottom: '0',
+  },
   },
   box: {
     // ...theme.paper,
@@ -171,6 +254,12 @@ const stepOneStyles = makeStyles(theme => ({
     width: "28%",
     padding: "2%",
     height: "45vh",
+    // For mobiles and smaller
+    ["@media (max-width:500px)"]: {
+      width:'86%',
+      height:'100%',
+      marginBottom: '0',
+  },
     "&.middle": {
       margin: "0 1%"
     }
@@ -178,12 +267,17 @@ const stepOneStyles = makeStyles(theme => ({
   image: {
     width: "100%",
     height: "auto",
-    objectFit: "contain"
+    objectFit: "contain",
   },
   tutorialH: {
     color: "black",
     letterSpacing: "0",
-    marginBottom: "20%"
+    marginBottom: "20%",
+     // For mobiles and smaller
+     ["@media (max-width:500px)"]: {
+      marginBottom:'5%',
+      marginTop:'3%'
+  },
   },
   tutorialSteps: {
     ...theme.typography.h6,
